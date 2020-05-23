bd6b2b1f64fa67c5d262e1b6ae1d4faa61a2a096
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-05-23 19:44:40 +0530

    rest uploadded

--- Deno_Rest_API/routes.ts
+++ Deno_Rest_API/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts'
 import { getProducts, getProduct, addProduct, updateProduct, deleteProduct } from './controller/product.ts'
+import { GetProducts as GP } from './DBController/product.ts';
 
 const router = new Router()
 
-router.get('/api/v1/products', getProducts)
+router.get('/api/v1/products', GP)
 router.get('/api/v1/products/:id', getProduct)
 router.post('/api/v1/products', addProduct)
 router.put('/api/v1/products/:id', updateProduct)
--- Deno_Rest_API/server.ts
+++ Deno_Rest_API/server.ts
@@ -10,11 +10,6 @@ const app=new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-router.get("/api/v1/product",({ response }:{ response:any })=>
-{
-    response.body='Hello world';
-})
-
 console.log(`Server Running on port ${port}`);
 
 await app.listen({ port })
